d7640a6c1acbae158a447c44c956afc5c654a6a3
Ahmed <ahmed.chabib16@gmail.com>
2021-05-04 11:21:41 +0200

    testing the algo & trying to find the right parameters for my image

--- brox_mod.py
+++ brox_mod.py
@@ -9,23 +9,25 @@ from scipy.ndimage import median_filter,gaussian_filter
 import scipy.sparse as sparse
 import math
 from scipy.ndimage import map_coordinates
+import matplotlib.pyplot as plt
 ##############################################
 def psi_data(I1,I2,I2x,I2y,du,dv,eps,a):
     ''' Computing psi_data'''
     dI=I2-I1+ I2x*du+I2y*dv
     dI=dI**2
-    #psip = 1/ math.sqrt(dI + eps**2);
-    psip = (dI + eps**2)**(a-1)
+    psip = 1/ np.sqrt(dI + eps**2);
+    #psip = (dI + eps**2)**(a-1)
     return psip
 ###########################################
 def psi_gradient(I1x,I1y,I2x,I2y,I2xx,I2yy, I2xy,du, dv,eps,a):
     ''' Compute psi_gradient'''
 
-	#psip = 1. / math.sqrt(dI2 + eps **2);
+	
     dIx = I2x- I1x+I2xx*du+I2xy*dv
     dIy = I2y- I1y+ I2xy * du + I2yy * dv
     dI2 = dIx * dIx + dIy * dIy
-    psip = (dI2 + eps**2)**(a-1)
+    #psip = (dI2 + eps**2)**(a-1)
+    psip = 1 / np.sqrt(dI2 + eps **2)
     return psip
 
 ###########################################
@@ -34,8 +36,8 @@ def psi_smooth(ux,uy,vx,vy,a,eps):
     du=ux**2+ uy**2
     dv= vx**2 + vy**2
     d2= du + dv
-    #psi= 1 / math.sqrt(d2 + eps**2)
-    psip = (d2 + eps**2)**(a-1)
+    psip= 1 / np.sqrt(d2 + eps**2)
+    #psip = (d2 + eps**2)**(a-1)
     return psip
 ###########################################
 def gradient(Im):
@@ -79,10 +81,10 @@ def image_interpolation(Im,u,v):
 ################################################
 def psi_divergence(psi):
     (n,m)=psi.shape
-    psi1=np.empty((n,m))
-    psi2=np.empty((n,m))
-    psi3=np.empty((n,m))
-    psi4=np.empty((n,m))
+    psi1=np.empty((n,m),dtype=np.float32)
+    psi2=np.empty((n,m),dtype=np.float32)
+    psi3=np.empty((n,m),dtype=np.float32)
+    psi4=np.empty((n,m),dtype=np.float32)
     #Center nodes 
     psi1[1:n-1,1:m-1]=psi[2:n,1:m-1]+psi[1:n-1,1:m-1]
     psi2[1:n-1,1:m-1]=psi[0:n-2,1:m-1]+psi[1:n-1,1:m-1]
@@ -91,8 +93,8 @@ def psi_divergence(psi):
     # First & last row
     psi1[0,1:m-1]=psi[1,1:m-1]+psi[0,1:m-1]
     psi2[0,1:m-1]=0
-    psi3[0,1:m-1]=psi[0,2:m]+psi[0-1,1:m-1]
-    psi4[0,1:m-1]=psi[0,0:m-2]+psi[0-1,1:m-1]
+    psi3[0,1:m-1]=psi[0,2:m]+psi[0,1:m-1]
+    psi4[0,1:m-1]=psi[0,0:m-2]+psi[0,1:m-1]
 
     psi1[n-1,1:m-1]=0
     psi2[n-1,1:m-1]=psi[n-2,1:m-1]+psi[n-1,1:m-1]
@@ -105,13 +107,13 @@ def psi_divergence(psi):
     psi4[1:n-1,0]=0
 
     psi1[1:n-1,m-1]=psi[2:n,m-1]+psi[1:n-1,m-1]
-    psi2[1:n-1,m-1]=psi[0:n-2,0]+psi[1:n-1,0]
+    psi2[1:n-1,m-1]=psi[0:n-2,m-1]+psi[1:n-1,m-1]
     psi3[1:n-1,m-1]=0
     psi4[1:n-1,m-1]=psi[1:n-1,m-2]+psi[1:n-1,m-1]
     #Corners
-    psi1[0,0]=psi[0,1]+psi[0,0]
+    psi1[0,0]=psi[1,0]+psi[0,0]
     psi2[0,0]=0; psi4[0,0]=0
-    psi3[0,0]=psi[1,0]+psi[0,0]
+    psi3[0,0]=psi[0,1]+psi[0,0]
 
     psi1[0,m-1]=psi[1,m-1]+psi[0,m-1]
     psi2[0,m-1]=0
@@ -126,7 +128,7 @@ def psi_divergence(psi):
     psi1[n-1,m-1]=0
     psi2[n-1,m-1]=psi[n-2,m-1]+psi[n-1,m-1]
     psi3[n-1,m-1]=0
-    psi4[n-1,m-1]=psi[n-1,m-1]+psi[n-1,m-2]
+    psi4[n-1,m-1]=psi[n-1,m-2]+psi[n-1,m-1]
 
     psi1=0.5*psi1
     psi2=0.5*psi2
@@ -147,7 +149,7 @@ def divergence_u(u,psi1,psi2,psi3,psi4):
 
     div_u[0,0]=psi1[0,0]*(u[1,0]-u[0,0])+psi3[0,0]*(u[0,1]-u[0,0]) #(0,0)
     div_u[0,m-1]=psi1[0,m-1]*(u[1,m-1]-u[0,m-1])+psi4[0,m-1]*(u[0,m-2]-u[0,m-1]) #(0,m-1)
-    div_u[n-1,0]=psi2[n-1,0]*(u[n-2,m-1]-u[n-1,0])+psi3[n-1,0]*(u[n-1,1]-u[n-1,0]) ##(n-1,0)
+    div_u[n-1,0]=psi2[n-1,0]*(u[n-2,0]-u[n-1,0])+psi3[n-1,0]*(u[n-1,1]-u[n-1,0]) ##(n-1,0)
     div_u[n-1,m-1]=psi2[n-1,m-1]*(u[n-2,m-1]-u[n-1,m-1])+psi4[n-1,m-1]*(u[n-1,m-2]-u[n-1,m-1]) ##(n-1,m-1)
     return div_u
 
@@ -159,96 +161,110 @@ def div_du(psi1,psi2,psi3,psi4,du):
 
     div_du[0,1:m-1]=psi1[0,1:m-1]*du[1,1:m-1]+psi3[0,1:m-1]*du[0,2:m]+psi4[0,1:m-1]*du[0,0:m-2]
     div_du[n-1,1:m-1]=psi2[n-1,1:m-1]*du[n-2,1:m-1]+psi3[n-1,1:m-1]*du[n-1,2:m]+psi4[n-1,1:m-1]*du[n-1,0:m-2]
-
     div_du[1:n-1,0]=psi1[1:n-1,0]*du[2:n,0]+psi2[1:n-1,0]*du[0:n-2,0]+psi3[1:n-1,0]*du[1:n-1,1]
     div_du[1:n-1,m-1]=psi1[1:n-1,m-1]*du[2:n,m-1]+psi2[1:n-1,m-1]*du[0:n-2,m-1]+psi4[1:n-1,m-1]*du[1:n-1,m-2]
-
     div_du[0,0]=psi1[0,0]*du[1,0]+psi3[0,0]*du[0,1]
     div_du[0,m-1]=psi1[0,m-1]*du[1,m-1]+psi4[0,m-1]*du[0,m-2]
     div_du[n-1,0]=psi2[n-1,0]*du[n-2,0]+psi3[n-1,0]*du[n-1,1]
     div_du[n-1,m-1]=psi2[n-1,m-1]*du[n-2,m-1]+psi4[n-1,m-1]*du[n-1,m-2]
+    '''div_du[0,1:m-1]=psi1[0,1:m-1]*du[1,1:m-1]+   psi2[0,1:m-1]*du[0,1:m-1]  +psi3[0,1:m-1]*du[0,2:m]+psi4[0,1:m-1]*du[0,0:m-2]
+    div_du[n-1,1:m-1]=  psi1[n-1,1:m-1]*du[n-1,1:m-1]   +psi2[n-1,1:m-1]*du[n-2,1:m-1]+psi3[n-1,1:m-1]*du[n-1,2:m]+psi4[n-1,1:m-1]*du[n-1,0:m-2]
+    div_du[1:n-1,0]=psi1[1:n-1,0]*du[2:n,0]+psi2[1:n-1,0]*du[0:n-2,0]+psi3[1:n-1,0]*du[1:n-1,1]    +psi4[1:n-1,0]*du[1:n-1,0]
+    div_du[1:n-1,m-1]=psi1[1:n-1,m-1]*du[2:n,m-1]+psi2[1:n-1,m-1]*du[0:n-2,m-1]+          psi3[1:n-1,m-1]*du[1:n-1,m-1]           +psi4[1:n-1,m-1]*du[1:n-1,m-2]
+    div_du[0,0]=psi1[0,0]*du[1,0]+   psi2[0,0]*du[0,0]+         psi4[0,0]*du[0,0]+psi3[0,0]*du[0,0]
+    div_du[0,m-1]=psi1[0,m-1]*du[1,m-1]+psi4[0,m-1]*du[0,m-2]    +psi2[0,m-1]*du[0,m-1]+psi3[0,m-1]*du[0,m-1]             
+    div_du[n-1,0]=psi2[n-1,0]*du[n-2,0]+psi3[n-1,0]*du[n-1,1]     +psi1[n-1,0]*du[n-1,0]+psi4[n-1,0]*du[n-1,0]
+    div_du[n-1,m-1]=psi2[n-1,m-1]*du[n-2,m-1]+psi4[n-1,m-1]*du[n-1,m-2]          +psi1[n-1,m-1]*du[n-1,m-1]+psi3[n-1,m-1]*du[n-1,m-1]'''
     return div_du
 
 ################################################
 
 def brox_opt_flow(Im1,Im2,u,v,gamma,alpha,inner_it,outer_it,a,eps,w,iter_SOR,tol):
     (n,m)=Im1.shape
-    [I1x,I1y]=gradient(Im1)
-    [I2x,I2y]=gradient(Im2)
-
-    [I2xx,I2yy,I2xy]=second_deriv(Im2)
+    [I1x,I1y]=gradient(Im1) #1
+    [I2x,I2y]=gradient(Im2) 
+    [I2xx,I2yy,I2xy]=second_deriv(Im2) #2
     for i in range(outer_it):
-        I2w=image_interpolation(Im2,u,v)
+        I2w=image_interpolation(Im2,u,v) #4
         I2xw=image_interpolation(I2x,u,v)
         I2yw=image_interpolation(I2y,u,v)
-        I2xxw=image_interpolation(I2xx,u,v)
+        I2xxw=image_interpolation(I2xx,u,v)#5
         I2yyw=image_interpolation(I2yy,u,v)
         I2xyw=image_interpolation(I2xy,u,v)
 
-        [ux,uy]=gradient(u)
+        [ux,uy]=gradient(u) #6
         [vx,vy]=gradient(v)
-        psis=psi_smooth(ux,uy,vx,vy,a,eps)
-        [psi1,psi2,psi3,psi4]=psi_divergence(psis)
+
+        psis=psi_smooth(ux,uy,vx,vy,a,eps) #7
+
+        [psi1,psi2,psi3,psi4]=psi_divergence(psis) #8
         div_u=divergence_u(u,psi1,psi2,psi3,psi4)
         div_v=divergence_u(v,psi1,psi2,psi3,psi4)
         div_d=alpha*(psi1+ psi2 + psi3 + psi4)
 
-        du=np.zeros(u.shape); dv=np.zeros(u.shape)
+        du=np.zeros(u.shape,dtype=np.float32); dv=np.zeros(u.shape,dtype=np.float32) #9 10 
 
         for j in range(inner_it):
-            psid=psi_data(Im1,I2w,I2xw,I2yw,du,dv,eps,a)
+            psid=psi_data(Im1,I2w,I2xw,I2yw,du,dv,eps,a) #12 
             psig=psi_gradient(I1x,I1y,I2xw,I2yw,I2xxw,I2yyw, I2xyw,du, dv,eps,a)
-            dif = I2w-Im1
+            dif = I2w-Im1 #13 
             dif_x=I2xw-I1x;dif_y=I2yw-I1y
 
             Au=-psid*dif*I2xw; Au=Au+alpha*div_u; Au=Au-gamma*psig*(dif_x*I2xxw+dif_y*I2xyw)
             Av=-psid*dif*I2yw; Av=Av+alpha*div_v; Av=Av-gamma*psig*(dif_x*I2xyw+dif_y*I2yyw)
 
-            Du=psid**I2xw**2; Du=Du+gamma*psig*(I2xxw**2+I2xyw**2); Du=Du+alpha*div_d
-            Dv=psid**I2yw**2; Dv=Dv+gamma*psig*(I2yyw**2+I2xyw**2); Dv=Dv+alpha*div_d
+            Du=psid*I2xw**2; Du=Du+gamma*psig*(I2xxw**2+I2xyw**2); Du=Du+div_d
+            Dv=psid*I2yw**2; Dv=Dv+gamma*psig*(I2yyw**2+I2xyw**2); Dv=Dv+div_d
             D=psid*I2xw*I2yw+gamma*psig*(I2xxw+I2yyw)*I2xyw
 
-            error=1000; sor_it=0
-            while((error >tol) or sor_it<iter_SOR ):
-                error=0; sor_it=sor_it+1
-
+            error=1; sor_it=0
+            while((error >tol) and sor_it<iter_SOR ):
                 div_duu=div_du(psi1,psi2,psi3,psi4,du)
                 div_dv=div_du(psi1,psi2,psi3,psi4,dv)
                 du0=du; dv0=dv
-                du= (1-w) * du + w * (Au - D * dv + alpha * div_duu) / Du
-                dv= (1-w) * dv + w * (Av - D * du + alpha * div_dv) / Dv
-                error=np.linalg.norm((du-du0))+np.linalg.norm((dv-dv0))
+                du= ( (1-w) * du + w * (Au - D * dv + alpha * div_duu) )/ Du #15
+                '''print('Du:',np.isnan(Du))
+                print('Dv:',np.isnan(Dv))'''
+                #print('du',du.min(),du.max())
+
+                dv=  ( (1-w) * dv + w * (Av - D * du + alpha * div_dv) ) / Dv #16
+                error=np.linalg.norm((du-du0))+np.linalg.norm((dv-dv0)) #17
                 error=math.sqrt(error/n*m)
-            
-            u=u+du; v=v+dv 
+                sor_it=sor_it+1 #18
+
+        '''du[du>1]=1; du[du<-1]=-1
+        dv[dv>1]=1; dv[dv<-1]=-1 '''
+  
+        u=u+du; v=v+dv #21 22
+        u=median_filter(u,(5,5))
+        v=median_filter(v,(5,5)) 
+        print('error',error)
         
     return[u,v]
-
-
-
-
-        
-
-
-
-
-
-    
 ################################################
-#A=np.floor(10*np.random.rand(4,4)) 
-'''A=[[6., 3., 4. ,8.,6.]
- ,[6. ,5., 3. ,1.,0.]
- ,[9. ,1. ,9., 5.,1.]
- ,[5., 8. ,4., 6.,7.]]
-A=np.array(A)
-print('A')
-print(A)
-[psi1,psi2,psi3,psi4]=psi_divergence(A)
-u=np.ones(A.shape); v=2*np.ones(A.shape)
-#u=np.random.rand(4,5)
-#div_u=divergence_u(u,psi1,psi2,psi3,psi4)
 
-[u,v]=brox_opt_flow(A,2*A,u,v,1,2,3,4,0.5,0.001,0.5,5,0.001)
+'''
+Im1=np.random.rand(2,3);
+Im2=np.random.rand(2,3);
+u=np.random.rand(2,3);
+v=np.random.rand(2,3);'''
 
-print(u)
-print(v)'''
+'''Im1=cv2.imread('Im1.png',0)
+Im2=cv2.imread('Im2.png',0)
+Im1=np.array(Im1,dtype=np.float32)
+Im2=np.array(Im2,dtype=np.float32)
+u=np.zeros((Im1.shape[0],Im1.shape[1]))
+v=u=np.zeros((Im1.shape[0],Im1.shape[1]))
+gamma=7
+alpha=18
+inner_it=20
+outer_it=10
+a=0.5
+eps=0.0001
+w=0.5
+iter_SOR=50
+tol=0.001
+nscales=3
+nu=0.5
+gaussian_sigma=0.9
+[u,v]=brox_opt_flow(Im1,Im2,u,v,gamma,alpha,inner_it,outer_it,a,eps,w,iter_SOR,tol)'''
--- brox_pyram.py
+++ brox_pyram.py
@@ -1,7 +1,8 @@
 import numpy as np
 from numpy.core.fromnumeric import reshape
 from math import floor
-
+from ssl import DefaultVerifyPaths
+import matplotlib.pyplot as plt
 
 import cv2
 from math import ceil,floor
@@ -13,21 +14,22 @@ import scipy.sparse as sparse
 import math
 from scipy.ndimage import map_coordinates
 import brox_mod as brox 
-import  scipy.misc 
+#import  scipy.misc 
+from skimage.transform import resize
 #####################################################################################
 def resize_function_CtoF(ns,Im,tab):
     '''(n,m): shape of the image Im & ns the pyramid level, reshape from a coarsest to  a finer level  '''
     (new_N,new_M)=tab[ns]
-    Ims=scipy.misc.imresize(Im,(new_N,new_M),interp='bilinear')
+    #Ims=scipy.misc.imresize(Im,(new_N,new_M),interp='bilinear')
+    Ims=resize(Im,(new_N,new_M))
     return Ims
 
 def resize_function_FtoC(u,tab,ns):
     '''(n,m): shape of the dispacement u & ns the pyramid level, reshape from a finer level to  the next coarsest one '''
-    '''(n,m)=u.shape
-    new_N=round(n/factor)
-    new_M=round(m/factor)'''
+
     (new_N,new_M)=tab[ns-1]
-    new_u=scipy.misc.imresize(u,(new_N,new_M),interp='bilinear')
+    #new_u=scipy.misc.imresize(u,(new_N,new_M),interp='bilinear')
+    new_u=resize(u,(new_N,new_M))
     return new_u
 
 def sizes(nscales,Im,factor):
@@ -40,7 +42,8 @@ def sizes(nscales,Im,factor):
         tab.append((new_N,new_M))
     return tab
 #####################################################################################
-def brox_method(Im1,Im2,u,v,gamma,alpha,inner_it,outer_it,a,eps,w,iter_SOR,tol,nscales,nu,gaussian_sigma):
+def brox_method(Im1,Im2,u,v,gamma,alpha,inner_it,outer_it,a,eps,w,iter_SOR,tol,nscales,nu,gaussian_sigma0):
+    gaussian_sigma=gaussian_sigma0*math.sqrt(1/nu**2-1)
     Im1=gaussian_filter(Im1,gaussian_sigma)
     Im2=gaussian_filter(Im2,gaussian_sigma)
     tab=sizes(nscales,Im1,nu)
@@ -52,6 +55,10 @@ def brox_method(Im1,Im2,u,v,gamma,alpha,inner_it,outer_it,a,eps,w,iter_SOR,tol,n
         print('level:',ns)
         if (ns!=0):
             Im1s=resize_function_CtoF(ns,Im1,tab); Im2s=resize_function_CtoF(ns,Im2,tab)
+            Im1s=gaussian_filter(Im1s,gaussian_sigma)
+            Im2s=gaussian_filter(Im2s,gaussian_sigma)
+            Im1s=np.array(Im1s,np.float32)
+            Im2s=np.array(Im2s,np.float32)
         if(ns==0):
             Im1s=Im1; Im2s=Im2
         print('Im shape',Im1s.shape)
@@ -59,39 +66,58 @@ def brox_method(Im1,Im2,u,v,gamma,alpha,inner_it,outer_it,a,eps,w,iter_SOR,tol,n
         if(ns!=0):
             u=resize_function_FtoC(u,tab,ns)
             v=resize_function_FtoC(v,tab,ns)
-
+            '''u=median_filter(u,(5,5))
+            v=median_filter(v,(5,5))'''
             u=1/nu*u
             v=1/nu*v
-    
-
+        print(u)
     return[u,v]
-
-
-
-
 ########################################################################################
-'''u=np.random.rand(5,3); 
-factor=0.5; ns=2
-#Ims=resize_function_CtoF(ns,Im,factor)
-new_u=resize_function_FtoC(u,factor)
-print(new_u.shape)'''
+#Im1=np.random.rand(20,30);
+#Im2=np.random.rand(20,30);
+#u=np.random.rand(20,30);
+#v=np.random.rand(20,30);
+Im1=cv2.imread('Im1.png',0)
+Im2=cv2.imread('Im2.png',0)
+Im1=np.array(Im1,dtype=np.float32)
+Im2=np.array(Im2,dtype=np.float32)
+u=np.zeros((Im1.shape[0],Im1.shape[1]))
+v=u=np.zeros((Im1.shape[0],Im1.shape[1]))
 
-Im1=np.random.rand(20,30);
-Im2=np.random.rand(20,30);
-u=np.random.rand(20,30);
-v=np.random.rand(20,30);
-'''u=np.zeros((40,50))
-v=np.zeros((40,50))'''
-gamma=0.5
-alpha=0.2
-inner_it=10
-outer_it=10
+'''gamma=20
+alpha=15
+inner_it=20
+outer_it=4
 a=0.5
-eps=0.001
+eps=0.0001
 w=0.5
-iter_SOR=30
-tol=0.01
-nscales=3
+iter_SOR=500
+tol=0.0001
+nscales=5
 nu=0.5
-gaussian_sigma=0.9
+gaussian_sigma=0.6'''
+gamma=30
+alpha=2
+inner_it=20
+outer_it=2
+a=0.5
+eps=0.0001
+w=1
+iter_SOR=700
+tol=0.0001
+nscales=6
+nu=0.5
+gaussian_sigma=0.6
 [u,v]=brox_method(Im1,Im2,u,v,gamma,alpha,inner_it,outer_it,a,eps,w,iter_SOR,tol,nscales,nu,gaussian_sigma)
+
+N,M=Im1.shape
+y=np.linspace(0,N-1,N)
+x=np.linspace(0,M-1,M)
+x,y=np.meshgrid(x,y)
+x2=x+u; y2=y+v
+x2=np.array(x2,dtype=np.float32)
+y2=np.array(y2,dtype=np.float32)
+I=cv2.remap(np.array(Im1,dtype=np.float32),x2,y2,cv2.INTER_LINEAR)
+norme=np.linalg.norm(I-Im2)/np.linalg.norm(Im2)
+print(norme)
+cv2.imwrite('I_2.png',I)
